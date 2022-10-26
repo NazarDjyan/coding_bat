1eec6021f20b483282ec2d75b64df50570500a25
Nazar Adnavochka <91146387+NazarDjyan@users.noreply.github.com>
2022-10-27 00:09:39 +0300

    Update IcyHot.java

--- Warmup-1/IcyHot.java
+++ Warmup-1/IcyHot.java
@@ -5,6 +5,5 @@
 //    icyHot(2, 120) → false
 
 public boolean icyHot(int temp1, int temp2) {
-    if ((temp1 < 0 && temp2 > 100) || (temp1 > 100 && temp2 < 0)) return true;
-    return false;
+    return ((temp1 < 0 && temp2 > 100) || (temp1 > 100 && temp2 < 0));
 }
