c9c25495d4604ee3e6eecd989604064c622b5b94
Nazar Adnavochka <91146387+NazarDjyan@users.noreply.github.com>
2022-10-27 00:18:45 +0300

    Update HasTeen.java

--- Warmup-1/HasTeen.java
+++ Warmup-1/HasTeen.java
@@ -6,9 +6,7 @@
 //    hasTeen(20, 10, 13) → true
     
 public boolean hasTeen(int a, int b, int c) {
-    return (
-                (a >= 13 && a <= 19) ||
+    return      (a >= 13 && a <= 19) ||
                 (b >= 13 && b <= 19) ||
-                (c >= 13 && c <= 19)
-            );
+                (c >= 13 && c <= 19);
 }
